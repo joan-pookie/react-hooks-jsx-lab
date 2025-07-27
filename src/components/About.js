4145fa9952646af8dfe8a4832c3e01b0aa95eb49
Joan Chepkoech <joan92311@gmail.com>
2025-07-27 08:42:26 +0300

    push changes

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>
+      I am a software developer with a passion for creating web applications.
+      I love working with React and exploring new technologies.
+    </p>
+  </div>;
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,12 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return(
+  <div id="home">
+    <h1 style={{ color: "firebrick" }}>Welcome to {name}'s Home Page</h1>
+    <p>{name} is a software developer living in {city}.</p>
+  </div>
+  );
 }
 
 export default Home;
